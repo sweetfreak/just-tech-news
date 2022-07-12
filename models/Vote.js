c3721c194f92f42da06d37b4892c8aa1a3fe70d9
jesse sheehan <jessetsheehan@gmail.com>
2022-07-12 12:42:06 -0400

    added voting api functionality and refactored code a little

--- models/Vote.js
+++ models/Vote.js
@@ -30,7 +30,7 @@ Vote.init(
     {
         sequelize,
         timestamps: false,
-        freeezeTableName: true,
+        freezeTableName: true,
         underscored: true,
         modelName: 'vote'
     }
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,31 +1,14 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
-//get all users
-// router.get('/', (req, res) => {
-//     console.log('====================');
-//     Post.findAll({
-//         //query configurtion
-//         attributes: ['id', 'post_url', 'title', 'created_at'],
-//         include: [
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//     .then(dbPostData => res.json(dbPostData))
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     })
-// })
 
 // get all users
 router.get('/', (req, res) => {
     //console.log('====================');
     Post.findAll({
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
       order: [['created_at', 'DESC']],
       include: [
         {
@@ -46,7 +29,8 @@ router.get('/', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
       include: [
         {
           model: User,
@@ -80,6 +64,49 @@ router.post('/', (req, res) => {
     res.status(500).json(err);
   })
 })
+
+//PUT /api/posts/upvote
+//MUST be before the: router.put('/:id) because otherwise it'll think '/upvote' is an id!
+router.put('/upvote', (req, res) => {
+
+  //custom statis method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+
+
+  // MOVED THE BELOW TO THE UPVOTE METHOD IN POST.js
+  // Vote.create({
+  //   user_id: req.body.user_id,
+  //   post_id: req.body.post_id
+  // }).then(() => {
+  //   // then find the post we just voted on
+  //   return Post.findOne({
+  //     where: {
+  //       id: req.body.post_id
+  //     },
+  //     attributes: [
+  //       'id',
+  //       'post_url',
+  //       'title',
+  //       'created_at',
+  //       // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+  //       [
+  //         sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+  //         'vote_count'
+  //       ]
+  //     ]
+  //   })
+  // .then(dbPostData => res.json(dbPostData))
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.status(400).json(err);
+  //   });
+  // });
+});
 
 router.put('/:id', (req, res) => {
   Post.update(
